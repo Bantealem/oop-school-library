0760f86de74df18d72631365c0acae750dd053ad
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-11-24 17:41:32 +0300

    Add more test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,4 +1,6 @@
 require_relative './../book'
+require './rental'
+require './person'
 
 describe Book do
   context 'when testing the book class' do
@@ -6,6 +8,10 @@ describe Book do
       book = Book.new('12 Rules', 'Peterson')
       expect(book.title).to eq '12 Rules'
       expect(book.author).to eq 'Peterson'
+    end
+    it 'it has add_rental method' do
+      book = Book.new('12 Rules', 'Peterson')
+      expect(book.add_rental(Person.new(67, 'John'), '11/24/2022')).to be_a(Rental)
     end
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,4 +1,7 @@
 require_relative './../person'
+require './book'
+require './rental'
+
 describe Person do
   context 'when testing the person class' do
     it 'should create a new person when person class instantiated' do
@@ -7,6 +10,14 @@ describe Person do
       expect(person.age).to eq 67
       expect(person.name).to eq 'John'
       expect(person.id).to eq id
+    end
+    it 'it has can_use_services? method' do
+      person = Person.new(67, 'John')
+      expect(person.can_use_services?).to eq true
+    end
+    it 'it has add_rental method' do
+      person = Person.new(67, 'John')
+      expect(person.add_rental('11/24/2022', Book.new('Title', 'Author'))).to be_a(Rental)
     end
   end
 end
